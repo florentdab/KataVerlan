b40be34ab4cbdab73d88c88e8333cf4e2331563b
Laurent Aubertin <laubertin1967@gmail.com>
2016-09-27 08:26:10 +0200

    add autoloadind, phpunit config, and final code

--- src/KataVerlan.php
+++ src/KataVerlan.php
@@ -4,11 +4,68 @@ namespace wcs; // or your own namespace
 
 class KataVerlan
 {
-    public static function action($value)
-    {
+    const CONSONNANT = "bcdfghjklmpqrstvwxz";
 
+    public static function action($value)   // simple version
+    {
+        $strWords = explode(" ", $value);
+        $newSentence = "";
+        foreach($strWords as $word){
+            $size = strlen($word);
+            if ($size >5){
+                $center = (int)($size / 2);
+                $left = substr($word, 0, $center);
+                $right = substr($word, $center, ($size - $center));
+                $newSentence .= $right . $left . " ";
+            } else {
+                $newSentence .= $word . " ";
+            }
+        }
+        return trim($newSentence);
+    }
+
+    public static function actionOption($value)     // more complex version
+    {
+        $strWords = explode(" ", $value);
+        $newSentence = "";
+        foreach($strWords as $word){
+            $size = strlen($word);
+            if ($size >5){
+                $center = self::getBestCenter($word, $size);
+                $left = substr($word, 0, $center);
+                $right = substr($word, $center, ($size - $center));
+                $newSentence .= $right . $left . " ";
+            } else {
+                $newSentence .= $word . " ";
+            }
+        }
+        return trim($newSentence);
+    }
+
+    private static function getBestCenter($word, $size){
+        $center = (int)($size / 2);
+        $increment = 0;
+        $phase = 1;
+        while (false === strpos(self::CONSONNANT, substr($word, ($center - 1) + $increment, 1))){
+            switch($phase){
+                case 1:
+                    $increment++;
+                    $phase++;
+                    break;
+                case 2:
+                    $increment = -$increment;
+                    $phase++;
+                    break;
+                case 3:
+                    $increment = abs($increment);
+                    $phase = 1;
+                    break;
+            }
+        }
+        return $center + $increment - 1;
     }
 }
 
-
-
+echo "La maison prés de la fontaine => "  . KataVerlan::actionOption("La maison prés de la fontaine") . "\n";
+echo "La moquette de demain => "  . KataVerlan::actionOption("La moquette de demain") . "\n";
+echo "Une voiture futuriste => "  . KataVerlan::actionOption("Une voiture futuriste") . "\n";
--- tests/KataVerlanTest.php
+++ tests/KataVerlanTest.php
@@ -4,7 +4,15 @@
   {
       public function testAction()
       {
-          $this->assertEquals(true, \wcs\KataVerlan::action("value"));
+          $this->assertEquals("la sonmai", \wcs\KataVerlan::action("la maison"));
+          $this->assertEquals("le chat gris", \wcs\KataVerlan::action("le chat gris"));
+          $this->assertEquals("la sonmai prés de la ainefont", \wcs\KataVerlan::action("la maison prés de la fontaine"));
       }
 
+      public function testActionOption()
+      {
+          $this->assertEquals("la sonmai", \wcs\KataVerlan::action("la maison"));
+          $this->assertEquals("le chat gris", \wcs\KataVerlan::action("le chat gris"));
+          $this->assertEquals("la sonmai prés de la tainefon", \wcs\KataVerlan::action("la maison prés de la fontaine"));
+      }
   }
